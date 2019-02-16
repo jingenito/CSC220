b001b9613b5344dac1d0a1dde740eea8ef567c30
Joe <jingenito1530@gmail.com>
2019-02-15 23:04:19 -0500

    messed around with helloworld project in eclipse

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,14 @@
+import doodlepad.*;
 
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Oval o = new Oval();
+		o.setFillColor(0,0,255);
+		o.setMouseClickedHandler(HelloWorld::oval_Click);
 	}
 	
+	public static void oval_Click(Shape shp, double x, double y, int button) {
+		System.out.println("Hello World");
+	}
 }
