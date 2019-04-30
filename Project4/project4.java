ea0f788ae1260a9c2ebc390d64129d16b3c9b377
Joe <jingenito1530@gmail.com>
2019-04-30 18:55:52 -0400

    Did work on project 4

--- Project4/project4.java
+++ Project4/project4.java
@@ -10,21 +10,76 @@ import java.io.*;
 
 public class project4
 {
+    public enum Results
+    {
+        P12("[12%] Genetic Privilege: Any exercise works for you"), 
+        P16("[16%] Genetic Privilege: Any diet works for you"), 
+        P39("[39%] Genetic Disprivilege: You will lose 2.5x as much weight on a low fat diet"), 
+        P45("[45%] Genetic Dispriviliege: You will lose 2.5x as much weight on a low carb diet"), 
+        P88("[88%] Genetic Disprivilege: Only high intensity exercise will help you lose weight");
+
+        private String ResultString;
+
+        Results(String s)
+        {
+            this.ResultString = s;
+        }
+
+        public String toString()
+        {
+            return ResultString;
+        }
+    }
+
+    public enum GeneLookup
+    {
+        a,b,c;
+
+        private String LookupString;
+
+        GeneLookup(String s)
+        {
+            this.LookupString = s;
+        }
+
+        public String toString()
+        {
+            return LookupString;
+        }
+    }
+
     public static void main(String[] args)
     {
         if(args.length < 1){ return;}
+
+        HashMap<String, String> genotypes = new HashMap<String, String>();
+
         try(Scanner sc = new Scanner(new File(args[0])))
         {
-            //read from file
-            while(sc.hasNext()) 
-            { 
-                String line = sc.nextLine();
-                
-            } 
+            genotypes = getHashMap(sc);
         }
-        catch(FileNotFoundException fnfEx)
+        catch(FileNotFoundException fNfEx)
         {
             System.out.println("Error: " + fNfEx);
         }
+
+        
+    }
+
+    private static HashMap<String, String> getHashMap(Scanner sc)
+    {
+        HashMap<String, String> h = new HashMap<String, String>();
+        while(sc.hasNext()) 
+        { 
+            String line = sc.nextLine();
+            String[] items = line.split("\t");
+            h.put(items[0], items[3]);
+        } 
+        return h;
+    }
+
+    private static Results getFlowChartResults(HashMap<String, String> genotypes)
+    {
+
     }
 }
