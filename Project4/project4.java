1bce41a97628bd4a92ead6f621cff415dd1c7ed4
Joe <jingenito1530@gmail.com>
2019-05-09 15:09:36 -0400

    added some of my own comments as well

--- Project4/project4.java
+++ Project4/project4.java
@@ -6,14 +6,12 @@
 // javac project4.java          (to compile)
 // java project4 report04.txt           (to run the code: needs a report in line next to it)
 
-//  imports all java methods of util and io
 import java.util.*;
 import java.io.*;
 
-//  the main class that this file's properties and methods fall under
 public class project4
 {
-    //  this makes a collection of constants under the category of Results; holds the String text for each Genetic Privelidge result
+    //  created a Results enum which stores the Result String for each possible percentage of genetic privilege/disprivilege determined from the flow chart
     public enum Results
     {
         P12("[12%] Genetic Privilege: Any exercise works for you"), 
@@ -22,23 +20,23 @@ public class project4
         P45("[45%] Genetic Dispriviliege: You will lose 2.5x as much weight on a low carb diet"), 
         P88("[88%] Genetic Disprivilege: Only high intensity exercise will help you lose weight");
 
-//  makes a string to hold the inputted result when calling this enum collection
+        //  string to store the result string for each output of the flow chart
         private String ResultString;
 
-//  this method makes whatever String is inputted into this enum collection move itself into the actual String of Results; allows enum constants to be read
+        //  Constructor method takes in the string representation as a parameter and assigns it to ResultString
         Results(String s)
         {
             this.ResultString = s;
         }
 
-//  this method allows the enum string constant to print out
+        //  Override toString method to print ResultString
         public String toString()
         {
             return ResultString;
         }
     }
 
-//  this makes a collection of constants under the category of Genes; holds the String text for each gene the program has to check for
+    //  created a GeneLookup enum which stores the key to the hashmap (genotype RSID) for each node checkpoint in the flow chart
     public enum GeneLookup
     {
         gene1("rs4994"), 
@@ -48,61 +46,66 @@ public class project4
         gene5("rs1042714"),
         gene6("rs1801282");
 
-//  makes a string to hold the inputted gene when calling this enum collection
+        //  string to store the genotype RSID when calling this enum collection
         private String LookupString;
 
-//   this method makes whatever String is inputted into this enum collection move itself into the actual String of Lookup; allows enum constants to be read
+        //   Constructor takes in the string representation as a parameter and assigns the value to LookupString
         GeneLookup(String s)
         {
             this.LookupString = s;
         }
-//  this method allows the enum string constant to print out
+        //  Override toString method to print LookupString
         public String toString()
         {
             return LookupString;
         }
     }
 
-//  the main method in this class that runs when the class is called
+    //  the main method in this class that runs when the class is called
     public static void main(String[] args)
     {
         //  if the project4 file is ran without a report file to read, then nothing will happen
         if(args.length < 1){ return;}
 
-//  makes a scanner that reads an inputted file on the String Array args zero'th spot
+        //  makes a scanner that reads the file name at index 0 of args
         try(Scanner sc = new Scanner(new File(args[0])))
         {
             //  allows the hashmap Genotypes to use the getHashMap's hashmap. This puts the scanners content from a hashmap into the offical Genotype one
             HashMap<String, String> genotypes = getHashMap(sc);
-            //  starts the checking process of the flowchart's paths: uses the Genotype hashmap's values according to the instructions on the original flowchart and then prints out all the information that is derived from the paths and enums
+            //  starts the flow chart algorithm: uses the Genotype hashmap's values according to the instructions on the original flowchart and then prints out all the information that is derived from the paths and enums
             FlowChart(genotypes);
         }
-        //  this statement says if the report file inputted into args[0] is not found, that there is an Error
+        //  this statement prints the string representation of the FileNotFoundException if caught
         catch(FileNotFoundException fNfEx)
         {
             System.out.println("Error: " + fNfEx);
         }
     }
 
-//  displays the use of the getHashMap mehtod which takes the scanner in it's parameters, which reads a report file after the "#" symbols so the scanner takes in only the data and puts it's ID and genotype in an hashmap
+    //  created getHashMap method which takes the scanner in it's parameters, which reads a report file after the "#" symbols so the scanner takes in only the data and puts it's ID and genotype in an hashmap
     private static HashMap<String, String> getHashMap(Scanner sc)
     {
-        //  the first hashmap that holds the scanner values
+        //  create a new hashmap that will be populated with the genotype RSID and value from the file opened by the scanner passed in
         HashMap<String, String> h = new HashMap<String, String>();
-        //  this whole loop says: if the scanner has another line to read on the file and it is not information followed by a "#" key, copy the ID and genotype into the hashmap
+        //  if the scanner has another line to read on the file and it is not information followed by a "#" key, copy the ID and genotype into the hashmap
         while(sc.hasNext()) 
         { 
             String line = sc.nextLine();
-            if(!line.contains("#"))
+            if(!line.startsWith("#"))
             {
                 String[] items = line.split("\t");
                 h.put(items[0], items[3]);
             }
         } 
-        return h;
+        //return the populated hashmap
+        return h; 
     }
 
-//  creates the method used for plugging in the Genotype hashmap values and providing the series of checks; which then derives the results
+    //  The algorithm followed by the flow chart can be broken into 7 nodes, 7th node being the end point of the algorithm.
+    //  The ouput of the check at the nth node is independent of any node previous to n, and therefore will be the same if any number
+    //  of nodes are checked previously to the nth node.
+    //  Following from this property, each node is broken up into 6 different functions in order to preserve code used for each node check point,
+    //  and make the code more readable and easier to debug.
     private static void FlowChart(HashMap<String, String> genotypes)
     {
         //  Check 1
@@ -116,7 +119,7 @@ public class project4
             Check3(genotypes);
         }
     }
-        //  Check 2
+    //  Check 2
     private static void Check2(HashMap<String, String> g)
     {
         if(g.get(GeneLookup.gene2.toString()).contains("AA") || g.get(GeneLookup.gene2.toString()).contains("TT"))
@@ -130,7 +133,7 @@ public class project4
             Check3(g);
         }
     }
-        //  Check 3
+    //  Check 3
     private static void Check3(HashMap<String, String> g)
     {
         if(g.get(GeneLookup.gene3.toString()).contains("GG"))
@@ -142,7 +145,7 @@ public class project4
             Check6(g);
         }
     }
-        //  Check 4
+    //  Check 4
     private static void Check4(HashMap<String, String> g)
     {
         if(g.get(GeneLookup.gene4.toString()).contains("CC"))
@@ -154,7 +157,7 @@ public class project4
             System.out.println(Results.P39);
         }
     }
-        //  Check 5
+    //  Check 5
     private static void Check5(HashMap<String, String> g)
     {
         if(g.get(GeneLookup.gene5.toString()).contains("CC"))
@@ -166,7 +169,7 @@ public class project4
             System.out.println(Results.P39);
         }
     }
-        //  Check 6
+    //  Check 6
     private static void Check6(HashMap<String, String> g)
     {
         if(g.get(GeneLookup.gene6.toString()).contains("CC"))
